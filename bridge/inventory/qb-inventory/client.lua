eeb4e08f3aed57bccd5f8ed4340f32577376dd24
taylorrenee1 <itstaylorrenee1@gmail.com>
2025-07-10 16:31:43 -0700

    chore(qb-inv/client): hasitem

--- bridge/inventory/qb-inventory/client.lua
+++ bridge/inventory/qb-inventory/client.lua
@@ -4,6 +4,23 @@ local core = {}
 
 local inventory = {}
 
+function inventory.hasItem(items, amount, metadata)
+    local amount, count = amount or 1, 0
+    for _, itemData in pairs(core.getPlayerData().items) do
+        if itemData and (itemData.name == items) then
+            printdb("HasItem: Item: %s Slot: %s x(%s)", tostring(items), itemData.slot, tostring(itemData.amount))
+            count += (itemData.amount or 1)
+        end
+    end
+    if count >= amount then
+        printdb("HasItem: FOUND %s / %s %s", count, amount, tostring(items))
+        return true
+    else
+        printwarn("HasItem: Items %s NOT FOUND", tostring(items))
+        return false
+    end
+end
+
 function inventory.getItemImage(item)
     return 'https://cfx-nui-qb-inventory/html/images/'..item..'.png'
 end
