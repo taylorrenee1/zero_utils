41ab9004636b94e076952b9f7f4611c21ca9d77f
taylorrenee1 <itstaylorrenee1@gmail.com>
2025-07-11 12:24:39 -0700

    chore(qb_inv): registerShop/ open shop

--- bridge/inventory/qb-inventory/server.lua
+++ bridge/inventory/qb-inventory/server.lua
@@ -1,7 +1,22 @@
 local inventory = {}
 
 function inventory.CreateUseableItem(item, func)
-    QBCore.Functions.CreateUseableItem(item, func)
+    return QBCore.Functions.CreateUseableItem(item, func)
+end
+
+function shops.registerShop(id, coords, label, items, society)
+    local storeData = {
+        name = id,
+        coords = coords,
+        label = label,
+        items = items,
+        society = society,
+    }
+    return exports['qb-inventory']:CreateShop(storeData)
+end
+
+function shops.openShop(id)
+    return exports['qb-inventory']:OpenShop(source, id)
 end
 
 return inventory
