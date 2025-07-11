5c8718eb4975ce418e72e45465f6b6f6d8d38e89
taylorrenee1 <itstaylorrenee1@gmail.com>
2025-07-10 20:08:47 -0700

    feat(core_loader): add qbx

--- shared/core_loader.lua
+++ shared/core_loader.lua
@@ -14,6 +14,15 @@ local core_definitions = {
             return Core
         end
     },
+    qbx_core = {
+        resource = "qbx-core",
+        loader = function()
+            if not next(Core) then
+                Core = exports['qbx-core']:GetCoreObject()
+            end
+            return Core
+        end
+    },
     ox_lib = {
         resource = "ox_lib",
         ignore_source = '@@ox_lib/init.lua',
