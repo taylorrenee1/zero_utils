99af1df032b0bfbfa76b84b83c0dfbde9e19ba2d
taylorrenee1 <itstaylorrenee1@gmail.com>
2025-07-22 20:48:07 -0700

    feat(utils/core/qbx_core/client): done, btu i probably did something wrong

---
+++ bridge/core/qbx_core/client.lua
@@ -0,0 +1,49 @@
+local core = {}
+
+function core.getPlayerData()
+    local player_data = exports.qbx_core:GetPlayerData()
+    if not player_data then return false, "Player Data not found" end
+    return player_data
+end
+
+function core.getJob()
+    local player_data, err = exports.qbx_core:GetPlayerData()
+    if not player_data then return false, err end
+    local job = player_data.job
+    if not job then return false, "Job not found" end
+    return job
+end
+
+function core.getJobData(jobName)
+    local Jobs = exports.qbx_core:GetJobs()
+    local job_data = Jobs[jobName]
+    if not job_data then return false, "Job not found" end
+    return job_data
+end
+
+function core.getGroupInfo(isJob)
+    local data = exports.qbx_core:GetPlayerData()
+    local group = isJob and data?.job or data?.gang
+    return {
+        name = group.name,
+        grade = group.grade.level,
+        label = group.label
+    }
+end
+
+function core.getGender()
+    local data = exports.qbx_core:GetPlayerData()
+    return (data.charinfo.gender or 0) + 1 -- 1 = Male, 2 = Female
+end
+
+function core.getGang()
+    local data, err = exports.qbx_core:GetPlayerData()
+    return data and data.gang or false, err or "Gang not found"
+end
+
+function core.isDead()
+    local data = exports.qbx_core:GetPlayerData()
+    return data.metadata and data.metadata.isdead
+end
+
+return core
