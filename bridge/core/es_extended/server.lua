2d352d9db726dc1d6a029168d3c9febb195fe990
taylorrenee1 <itstaylorrenee1@gmail.com>
2025-07-23 20:29:13 -0700

    feat(core/esx): added getPlayer and GetPlayerData functions

--- bridge/core/es_extended/server.lua
+++ bridge/core/es_extended/server.lua
@@ -1,5 +1,30 @@
 local core = {}
 
+function core.getPlayer(src)
+    local player = ESX.GetPlayerFromId(src)
+    if not player then return false, "Player not found" end
+    return player
+end
+
+function core.getPlayerData(src)
+    local player, err = ESX.GetPlayerFromId(src)
+    if not player then return false, err end
+    local player_data = {
+        name = player.getName(),
+        cash = player.getMoney(),
+        bank = player.getAccount("bank").money,
+
+        firstname = player.variables.firstName,
+        lastname = player.variables.lastName,
+
+        source = player.source,
+        job = player.job.name,
+        onDuty = player.job.onDuty,
+        citizenId = player.identifier,
+    }
+    if not player_data then return false, "Player Data not found" end
+    return player_data
+end
 
 function core.setThirst(src, thirst)
     TriggerClientEvent('esx_status:add', src, 'thirst', thirst)
