b0a260cf1cc249e9eb346afcf96f37359eae017f
taylorrenee1 <itstaylorrenee1@gmail.com>
2025-07-23 21:52:23 -0700

    feat(inventory/esx/client): getinv

---
+++ bridge/inventory/es_extended/client.lua
@@ -0,0 +1,29 @@
+local inventory = {}
+
+function inventory.getInv()
+    local Player  = ESX.getPlayerData()
+    return Player.inventory
+end
+
+function inventory.hasItem(items, amount, metadata)
+    local amount, count = amount or 1, 0
+    for _, itemData in pairs(QBCore.getPlayerData().items) do
+        if itemData and (itemData.name == items) then
+            printdb("HasItem: Item: %s Slot: %s x(%s)", tostring(items), itemData.slot, tostring(itemData.amount))
+            count += (itemData.amount or 1)
+        end
+    end
+    if count >= amount then
+        printdb("HasItem: FOUND %s / %s %s", count, amount, tostring(items))
+        return true
+    else
+        printwarn("HasItem: Items %s NOT FOUND", tostring(items))
+        return false
+    end
+end
+
+function inventory.getItemImage(item)
+    return 'https://cfx-nui-qb-inventory/html/images/'..item..'.png'
+end
+
+return inventory
