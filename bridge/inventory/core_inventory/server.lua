1589aca6155c018a505e994edeb5b09620f2130a
taylorrenee1 <itstaylorrenee1@gmail.com>
2025-07-24 11:47:14 -0700

    feat(inventory/core): hasitem

--- bridge/inventory/core_inventory/server.lua
+++ bridge/inventory/core_inventory/server.lua
@@ -8,4 +8,22 @@ function inventory.getInv(src)
     return PlayerInv
 end
 
+function inventory.hasItem(src, item, count, metadata)
+    local PlayerInv = inventory.getInv(src)
+    local required = count or 1
+    local found = 0
+
+    for _, v in pairs(PlayerInv) do
+        if v.name == item and (not metadata or v.metadata == metadata) then
+            found = found + (v.count or v.amount or 1)
+        end
+    end
+
+    if found >= required then
+        return true
+    else
+        return false, ("You do not have enough: %s"):format(item)
+    end
+end
+
 return inventory
