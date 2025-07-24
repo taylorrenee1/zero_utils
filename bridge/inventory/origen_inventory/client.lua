160ece7108ec80eb5211cfb17dafac7e47783439
taylorrenee1 <itstaylorrenee1@gmail.com>
2025-07-24 11:47:25 -0700

    feat(inventory/origen): hasitem

--- bridge/inventory/origen_inventory/client.lua
+++ bridge/inventory/origen_inventory/client.lua
@@ -8,4 +8,18 @@ function inventory.getInv()
     return PlayerInv
 end
 
+function inventory.hasItem(item, count, metadata)
+    local PlayerInv = inventory.getInv()
+    local required = count or 1
+    local found = 0
+
+    for _, v in pairs(PlayerInv) do
+        if v.name == item and (not metadata or v.metadata == metadata) then
+            found = found + (v.count or v.amount or 1)
+        end
+    end
+
+    return found >= required, (found >= required) and nil or ("You do not have enough: %s"):format(item)
+end
+
 return inventory
