21b9e381136d18f641564c72dc729c7a261c1aa5
taylorrenee1 <itstaylorrenee1@gmail.com>
2025-07-24 11:48:56 -0700

    feat(inventory/qsinv): hasitem

--- bridge/inventory/qs-inventory/client.lua
+++ bridge/inventory/qs-inventory/client.lua
@@ -1,13 +1,26 @@
 local inventory = {}
 
 
-
-function inventory.getInv(src)
-    local PlayerInv = exports['qs-inventory']:getUserInventory(src)
+function inventory.getInv()
+    local PlayerInv = exports['qs-inventory']:getUserInventory()
     if not PlayerInv then
         return {}
     end
-    return PlayerInv
+    return PlayerInv or {}
+end
+
+function inventory.hasItem(item, count, metadata)
+    local PlayerInv = inventory.getInv()
+    local required = count or 1
+    local found = 0
+
+    for _, v in pairs(PlayerInv) do
+        if v.name == item and (not metadata or v.metadata == metadata) then
+            found = found + (v.count or v.amount or 1)
+        end
+    end
+
+    return found >= required, (found >= required) and nil or ("You do not have enough: %s"):format(item)
 end
 
 return inventory
