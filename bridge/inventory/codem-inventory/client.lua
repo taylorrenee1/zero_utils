c70d73a4685bfe379fe6235b39b10a39283c2c2c
taylorrenee1 <itstaylorrenee1@gmail.com>
2025-07-24 11:47:07 -0700

    feat(inventory/codem): hasitem

--- bridge/inventory/codem-inventory/client.lua
+++ bridge/inventory/codem-inventory/client.lua
@@ -5,4 +5,26 @@ function inventory.getInv()
     return Inventory or {}
 end
 
+function inventory.hasItem(item, count, metadata)
+    local playerInv = inventory.getInv()
+    local required = count or 1
+    local found = 0
+
+    for _, v in pairs(playerInv) do
+        if v.name == item and (not metadata or v.metadata == metadata) then
+            found = found + (v.count or v.amount or 1)
+        end
+    end
+
+    if found >= required then
+        return true
+    else
+        return false, ("You do not have enough: %s"):format(item)
+    end
+end
+
+function inventory.getItemImage(item)
+    return 'https://cfx-nui-codem-inventory/html/images/'..item..'.png'
+end
+
 return inventory
--- bridge/inventory/codem-inventory/server.lua
+++ bridge/inventory/codem-inventory/server.lua
@@ -5,5 +5,23 @@ function inventory.getInv(src)
     return plyrinv or {}
 end
 
+function inventory.hasItem(src, item, count, metadata)
+    local playerInv = inventory.getInv(src)
+    local required = count or 1
+    local found = 0
+
+    for _, v in pairs(playerInv) do
+        if v.name == item and (not metadata or v.metadata == metadata) then
+            found = found + (v.count or v.amount or 1)
+        end
+    end
+
+    if found >= required then
+        return true
+    else
+        return false, ("You do not have enough: %s"):format(item)
+    end
+end
+
 
 return inventory
