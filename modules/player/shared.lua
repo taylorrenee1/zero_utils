9a9e1d9cce3f1c212893ab45765f967e227be20c
taylorrenee1 <itstaylorrenee1@gmail.com>
2025-07-30 11:09:21 -0700

    chore(modeules/playeR): add getjob/getgroup/getgender

--- modules/player/shared.lua
+++ modules/player/shared.lua
@@ -14,4 +14,18 @@ function zutils.player.getJob(src)
     return player.getJob(src)
 end
 
+function zutils.player.getJobData(src, jobName)
+    return player.getJobData(src, jobName)
+end
+
+function zutils.player.getGroupInfo(src, isJob)
+    return player.getPlayerData(src)
+end
+
+function zutils.player.getGender(src)
+    return player.getGender(src)
+end
+
+
+
 return zutils.player
