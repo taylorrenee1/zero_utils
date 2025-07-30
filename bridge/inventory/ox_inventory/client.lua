2e9371d1abba8d05bf89f978a2d3d9aad7d79cb7
taylorrenee1 <itstaylorrenee1@gmail.com>
2025-07-30 10:19:00 -0700

    chore(inventory/ox): getiteminfo update zero did

--- bridge/inventory/ox_inventory/client.lua
+++ bridge/inventory/ox_inventory/client.lua
@@ -7,11 +7,12 @@ function inventory.getItemInfo(item)
         return false, "Item does not exist: " .. item
     end
     return {
-        name = ox_item.name,
+        name = item,
         label = ox_item.label,
+        image = ox_item.client and ox_item.client.image or ox_item.name .. ".png",
         description = ox_item.description,
         weight = ox_item.weight,
-        stackable = ox_item.stackable,
+        stackable = ox_item.stack,
         metadata = ox_item.metadata or {},
     }
 end
