a67d130d9b10a1a8a68670b60430296d201f5b4d
taylorrenee1 <itstaylorrenee1@gmail.com>
2025-07-30 10:20:08 -0700

    chore(inventory/qb):  updates zero did

--- bridge/inventory/qb-inventory/client.lua
+++ bridge/inventory/qb-inventory/client.lua
@@ -1,3 +1,4 @@
+local qb_inventory = exports['qb-inventory']
 local inventory = {}
 
 function inventory.hasItem(items, amount, metadata)
@@ -26,6 +27,26 @@ function inventory.getInv()
         return {}
     end
     return PlayerInv
+end
+
+function inventory.getItemInfo(item)
+    local qb_item = QBCore.Shared.Items[item]
+    if not qb_item then
+        return false, "Item does not exist: " .. item
+    end
+    return {
+        name = qb_item.name,
+        label = qb_item.label,
+        image = qb_item.image,
+        description = qb_item.description,
+        weight = qb_item.weight,
+        stackable = qb_item.stackable,
+        metadata = qb_item.metadata or {},
+    }
+end
+
+function inventory.openInventory(inv_type, id)
+    TriggerServerEvent("zero_utils:server:qb-inventory:OpenInventory", inv_type, id)
 end
 
 return inventory
--- bridge/inventory/qb-inventory/server.lua
+++ bridge/inventory/qb-inventory/server.lua
@@ -1,8 +1,71 @@
+local qb_inventory = exports['qb-inventory']
 local inventory = {}
+local stashes = {}
 
 function inventory.CreateUseableItem(item, func)
     return QBCore.Functions.CreateUseableItem(item, func)
 end
+
+function inventory.getItemInfo(item)
+    local qb_item = QBCore.Shared.Items[item]
+    if not qb_item then
+        return false, "Item does not exist: " .. item
+    end
+    return {
+        name = qb_item.name,
+        label = qb_item.label,
+        image = qb_item.image,
+        description = qb_item.description,
+        weight = qb_item.weight,
+        stackable = qb_item.stackable,
+        metadata = qb_item.metadata or {},
+    }
+end
+
+function inventory.registerStash(id, label, slots, max_weight, owner, groups, coords)
+    stashes[id] = {
+        label = label,
+        slots = slots,
+        max_weight = max_weight,
+        owner = owner,
+        groups = groups,
+        coords = coords
+    }
+end
+
+RegisterNetEvent('zero_utils:server:qb-inventory:OpenInventory', function(inv_type, id)
+    local src = source
+    if inv_type == 'stash' and stashes[id] then
+        local stash = stashes[id]
+        if not stash then
+            printerr("Stash not found: " .. id)
+        end
+
+        local player_job = zutils.player.getJob(src).name or nil
+        local player_gang = zutils.player.getGang(src).name or nil
+
+        if type(stash.groups) == "string" then
+            stash.groups = { stash.groups }
+        end
+
+        if not (table.contains(stash.groups, player_job) or table.contains(stash.groups, player_gang) )then
+            return
+        end
+
+        if stash.coords then
+            local player_coords = GetEntityCoords(GetPlayerPed(src))
+            if #(player_coords - stash.coords) > 5.0 then
+                return
+            end
+        end
+
+        qb_inventory:OpenInventory(src, id, {
+            label = stash.label,
+            slots = stash.slots,
+            max_weight = stash.max_weight,
+        })
+    end
+end)
 
 function shops.registerShop(id, coords, label, items, society)
     local storeData = {
@@ -25,6 +88,23 @@ function inventory.getInv(src)
         return {}
     end
     return PlayerInv
+end
+
+function inventory.hasItem(src, item, count, metadata)
+    local PlayerInv = inventory.getInv(src)
+    if not PlayerInv then
+        return false, "You do not have enough: " .. item
+    end
+
+    local result = 0
+    for _, v in pairs(PlayerInv) do
+        if v.name == item and (not metadata or v.info.metadata == metadata) then
+            result = result + v.amount
+        end
+    end
+
+    if result >= (count or 1) then return true end
+    return false, "You do not have enough: " .. item
 end
 
 return inventory
