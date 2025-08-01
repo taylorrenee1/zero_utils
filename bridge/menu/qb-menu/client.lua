f0b00c610f2f0f0bdf69450d6ad0e6199502a8fa
taylorrenee1 <itstaylorrenee1@gmail.com>
2025-08-01 11:38:30 -0700

    feat(menu/qbmenu): add qb menu

---
+++ bridge/menu/qb-menu/client.lua
@@ -0,0 +1,44 @@
+local menu = {}
+
+function menu.registerMenu(id, data)
+    local Menu = {}
+
+    if data.options then
+        for _, opt in pairs(data.options) do
+            table.insert(Menu, {
+                header = opt.title or opt.label or opt.header or " ",
+                txt = opt.description or opt.txt or "",
+                icon = opt.icon,
+                isMenuHeader = opt.readOnly,
+                params = {
+                    event = opt.event,
+                    args = opt.args
+                }
+            })
+        end
+    end
+
+    if data.title then
+        table.insert(Menu, 1, {
+            header = data.title,
+            txt = data.subtitle or "",
+            isMenuHeader = true
+        })
+    end
+
+    exports['qb-menu']:openMenu(Menu)
+end
+
+function menu.openMenu(id)
+    -- Not needed for qb-menu
+end
+
+function menu.inputDialog(header, inputs)
+    return exports['qb-input']:ShowInput({
+        header = header,
+        submitText = "Submit",
+        inputs = inputs
+    })
+end
+
+return menu
