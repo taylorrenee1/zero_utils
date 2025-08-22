21280b8e7a281e2a4a9310c7c459c667743fcc6b
taylorrenee1 <itstaylorrenee1@gmail.com>
2025-08-22 14:38:49 -0700

    chore(qb-inv): shops upod

--- bridge/shop/qb-inventory/server.lua
+++ bridge/shop/qb-inventory/server.lua
@@ -18,8 +18,6 @@ function shop.registerShop(id, options)
         label = options.label or "Shop",
         coords = options.coords,
         items = formatItems(options.inventory),
-        slots = #options.inventory,
-        society = options.society,
     })
 end
 
@@ -29,21 +27,5 @@ RegisterNetEvent("zero_utils:server:qb-inventory:OpenShop", function(id)
         inventory:OpenShop(src, id)
     end
 end)
-
---[[ function shop.registerShop(id, coords, label, items, society)
-    local storeData = {
-        name = id,
-        coords = coords,
-        label = label,
-        slots = #items,
-        items = items,
-        society = society,
-    }
-    return exports['qb-inventory']:CreateShop(storeData)
-end ]]
-
-function shop.openShop(id)
-    return exports['qb-inventory']:OpenShop(source, id)
-end
 
 return shop
