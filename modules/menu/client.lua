46821bb0b92fff2acd1b3c2d8013fbd8ed4718f3
taylorrenee1 <itstaylorrenee1@gmail.com>
2025-08-22 14:54:37 -0700

    chore(): zero fixes

--- modules/menu/client.lua
+++ modules/menu/client.lua
@@ -3,24 +3,29 @@ if not menu then return end
 
 zutils.menu = {}
 
-zutils.menu.registerContext = function(id, menuData)
+zutils.menu.registerMenu = function(id, menuData)
     assert(id, "Menu ID must be specified")
     assert(menuData, "Menu data must be specified")
-    menu.registerContext(id, menuData)
+    menu.registerMenu(id, menuData)
 end
+zutils.menu.registerContext = zutils.menu.registerMenu
 
 zutils.menu.openMenu = function(id, menuData)
     assert(id, "Menu ID must be specified")
     if menuData then
-        zutils.menu.registerContext(id, menuData)
+        zutils.menu.registerMenu(id, menuData)
     end
-    menu.openMenu(id, menuData)
+    menu.openMenu(id)
 end
 
 zutils.menu.inputDialog = function(header, inputs)
     assert(header, "Header must be specified")
     assert(inputs, "Inputs must be specified")
     return menu.inputDialog(header, inputs)
+end
+
+zutils.menu.closeMenu = function()
+    menu.closeMenu()
 end
 
 return zutils.menu
--- modules/target/client.lua
+++ modules/target/client.lua
@@ -1,36 +1,49 @@
 local target = zutils.bridge_loader("target", "client")
 if not target then return end
 
+local targets = {}
+
 zutils.target = {}
 
 function zutils.target.addBoxZoneTarget(id, coords, size, options)
-    target.addBoxZoneTarget(id, coords, size, options)
+    targets[#targets+1] = target.addBoxZoneTarget(id, coords, size, options)
 end
+
 zutils.target.AddBoxZoneTarget = zutils.target.addBoxZoneTarget -- Alias for compatibility
 
 function zutils.target.addEntityTarget(entities, options)
-    target.addEntityTarget(entities, options)
+    targets[#targets+1] = target.addEntityTarget(entities, options)
 end
 zutils.target.AddEntityTargets = zutils.target.addEntityTarget -- Alias for compatibility
 
 function zutils.target.addNetIDTarget(netID, options)
-    target.addNetIDTarget(netID, options)
+    targets[#targets+1] = target.addNetIDTarget(netID, options)
 end
 zutils.target.AddNetIDTarget = zutils.target.addNetIDTarget -- Alias for compatibility
 
 function zutils.target.removeNetIDTarget(netId, options)
-    target.removeNetIDTarget(netId, options)
+    targets[#targets+1] = target.removeNetIDTarget(netId, options)
 end
 zutils.target.RemoveEntityTarget = zutils.target.removeNetIDTarget -- Alias for compatibility
 
 function zutils.target.addModelTarget(mode, options)
-    target.addModelTarget(mode, options)
+    targets[#targets+1] = target.addModelTarget(mode, options)
 end
 zutils.target.AddModelTarget = zutils.target.addModelTarget -- Alias for compatibility
 
 function zutils.target.removeModelTarget(model, options)
-    target.removeModelTarget(model, options)
+    targets[#targets+1] = target.removeModelTarget(model, options)
 end
 zutils.target.RemoveModelTarget = zutils.target.removeModelTarget -- Alias for compatibility
+
+
+AddEventHandler('onResourceStop', function(resource)
+    if resource ~= GetCurrentResourceName() then return end
+    for _, t in ipairs(targets) do
+        if t and t.remove then
+            t:remove()
+        end
+    end
+end)
 
 return zutils.target
