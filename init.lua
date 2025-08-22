3b814c24b70538854cb954ef825b01819e1449a6
taylorrenee1 <itstaylorrenee1@gmail.com>
2025-08-22 14:40:08 -0700

    cache

--- init.lua
+++ init.lua
@@ -90,4 +90,5 @@ zutils.require('/shared/utils.lua')
 zutils.require('/shared/core_loader.lua')
 
 zutils.initialized = true
+local _ = zutils.cache
 printdb("zero_utils initialized: %s", zutils.initialized)
---
+++ modules/cache/shared.lua
@@ -0,0 +1,118 @@
+local cache = {}
+local localCache = {}
+local isMainResource = zutils.name == 'zero_utils'
+
+if isMainResource then
+    function cache.set(key, value)
+        local oldValue = localCache[key]
+        if oldValue ~= value then
+            localCache[key] = value
+            TriggerEvent(('zutils:cache:%s'):format(key), value, oldValue)
+        end
+    end
+
+    exports('setCacheValue', cache.set)
+
+    function cache.get(key)
+        return localCache[key]
+    end
+
+    exports('getCacheValue', cache.get)
+
+    function cache.getAll()
+        return localCache
+    end
+
+    exports('getAllCache', cache.getAll)
+
+    if zutils.context == "client" then
+        CreateThread(function()
+            while true do
+                local ped = PlayerPedId()
+                cache.set('ped', ped)
+    
+                local vehicle = GetVehiclePedIsIn(ped, false)
+                if vehicle > 0 then
+                    if vehicle ~= cache.vehicle then
+                        cache.set("seat", false)
+                    end
+    
+                    if not cache.seat or GetPedInVehicleSeat(vehicle, cache.seat) ~= ped then
+                        for i = -1, GetVehicleMaxNumberOfPassengers(vehicle) - 1 do
+                            if GetPedInVehicleSeat(vehicle, i) == ped then
+                                cache.set('seat', i)
+                                break
+                            end
+                        end
+                    end
+    
+                    cache.set("vehicle", vehicle)
+                else
+                    cache.set("seat", false)
+                    cache.set("vehicle", false)
+                end
+    
+                Wait(100)
+            end
+        end)
+    end
+else
+    function cache.set(key, value)
+        if type(value) == 'function' then
+            value = value()
+        end
+        exports.zero_utils:setCacheValue(key, value)
+    end
+
+    function cache.get(key)
+        if localCache[key] ~= nil then
+            return localCache[key]
+        end
+
+        local value = exports.zero_utils:getCacheValue(key)
+        if value ~= nil then
+            localCache[key] = value
+        end
+
+        return value
+    end
+
+    function cache.getAll()
+        return exports.zero_utils:getAllCache()
+    end
+end
+
+function cache.onSet(key, callback)
+    if type(callback) ~= 'function' then
+        return printerr('Cache listener callback must be a function')
+    end
+
+    local eventName = ('zutils:cache:%s'):format(key)
+    
+    local handler = AddEventHandler(eventName, function(newValue, oldValue)
+        localCache[key] = newValue
+        callback(newValue, oldValue)
+    end)
+    return function()
+        RemoveEventHandler(handler)
+    end
+end
+
+setmetatable(cache, {
+    __index = function(self, key)
+        local func = rawget(self, key)
+        if func then return func end
+
+        return localCache[key] or  self.get(key)
+    end,
+
+    __newindex = function(self, key, value)
+        if type(value) == 'function' then
+            rawset(self, key, value)
+        else
+            self.set(key, value)
+        end
+    end
+})
+
+return cache
