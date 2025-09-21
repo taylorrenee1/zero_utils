d078c2e60c21a213920c61cfa1058af688998323
taylorrenee1 <itstaylorrenee1@gmail.com>
2025-09-20 20:56:35 -0700

    client/jobs.lua: jobu

---
+++ client/jobs.lua
@@ -0,0 +1,21 @@
+local core = zutils.bridge_loader("core", "client")
+if not core then return end
+
+zutils.cache.job = {
+    name = "",
+    onduty = false,
+    grade = 0,
+    isboss = false,
+}
+zutils.cache.onduty = false
+
+core.event.onJobUpdated(function(job)
+    zutils.cache.job = {
+        name = job.name,
+        onduty = job.onduty,
+        grade = job.grade.level,
+        isboss = job.grade.isboss or false,
+    }
+    zutils.cache.onduty = job.onduty
+end)
+
