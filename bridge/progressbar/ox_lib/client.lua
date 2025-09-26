dc01907bfef843792fdd07a435846ca2643a52d6
taylorrenee1 <itstaylorrenee1@gmail.com>
2025-09-26 15:21:06 -0700

    fix(ox_lib): fix ox_lib progress abr

--- bridge/progressbar/ox_lib/client.lua
+++ bridge/progressbar/ox_lib/client.lua
@@ -1,39 +1,52 @@
-local progress = function(name, label, duration, options)
-    local data = {
-        duration = duration,
-        label = label,
-        useWhileDead = options.useWhileDead,
-        canCancel = options.canCancel,
-        disable = {
-            move = options.controlDisables.move or false,
-            combat = options.controlDisables.combat or false,
-            car = options.controlDisables.car or false,
-            mouse = options.controlDisables.mouse or false,
-            sprint = options.controlDisables.sprint or false,
-        },
-        anim = {
-            dict = options.animation.animDict,
-            clip = options.animation.anim,
-            flag = options.animation.flags or 15,
-            blendIn = options.animation.blendIn or nil,
-            blendOut = options.animation.blendOut or nil,
-            duration = options.animation.duration or nil,
-            playbackRate = options.animation.playbackRate or nil,
-            lockX = options.animation.lockX or false,
-            lockY = options.animation.lockY or false,
-            lockZ = options.animation.lockZ or false,
-            scenario = options.animation.task,
-            playEnter = options.animation.playEnter or false,
-        },
-        prop = options.prop or options.propTwo,
-    }
+zutils.core_loader("ox_lib")
 
+local progress = function(name, label, duration, options)
+    options = options or {}
+
+    local data = {
+            duration = duration or 0,
+            label = label or "",
+            useWhileDead = options.useWhileDead,
+            canCancel = options.canCancel,
+            disable = {
+                move = (options.controlDisables.move and options.controlDisables.move) or false,
+                combat = (options.controlDisables.combat and options.controlDisables.combat) or false,
+                car = (options.controlDisables.car and options.controlDisables.car) or false,
+                mouse = (options.controlDisables.mouse and options.controlDisables.mouse) or false,
+                sprint = (options.controlDisables.sprint and options.controlDisables.sprint) or false,
+            },
+        }
+
+        local anim = options.animation
+        if anim and (anim.animDict or anim.dict or anim.task) then
+            data.anim = {
+                dict = anim.animDict or anim.dict,
+                clip = anim.anim or anim.clip,
+                flag = anim.flags or anim.flag or 15,
+                blendIn = anim.blendIn,
+                blendOut = anim.blendOut,
+                duration = anim.duration,
+                playbackRate = anim.playbackRate,
+                lockX = anim.lockX or false,
+                lockY = anim.lockY or false,
+                lockZ = anim.lockZ or false,
+                scenario = anim.task,
+                playEnter = (anim.playEnter ~= nil) and anim.playEnter or true,
+            }
+        end
+
+        if options.prop or options.propTwo then
+            if options.prop and options.propTwo then
+                data.prop = { options.prop, options.propTwo }
+            else
+                data.prop = options.prop or options.propTwo
+            end
+        elseif type(options.props) == "table" then
+            data.prop = options.props
+        end
+        local result
     if options.style == "circle" then
         data.position = options.position or "middle"
-    end
-
-    local result
-    if options.style == "circle" then
         result = lib.progressCircle(data)
     else
         result = lib.progressBar(data)
